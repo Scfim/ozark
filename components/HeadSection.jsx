5bc6cd21fc56e94facc87d2f17e30a8330982466
Aggestor <aggeer.aa@gmail.com>
2021-08-12 16:06:39 +0200

    usses solve

--- components/HeadSection.jsx
+++ components/HeadSection.jsx
@@ -1,9 +1,5 @@
 import React, { useState,useRef } from "react";
-<<<<<<< HEAD
 import { primaryColor,primaryBg } from "../styles/App.module.css";
-=======
-import { primaryColor } from "../styles/App.module.css";
->>>>>>> 6b022236e440ed7b9e5c6ae7d83c35ea92df14a5
 import { BiUser } from "react-icons/bi";
 import PropTypes from "prop-types";
 import { FaCamera } from "react-icons/fa";
@@ -36,7 +32,6 @@ export default function HeadSection({
           <img
             src={userImage}
             alt="user icon"
-<<<<<<< HEAD
             className="w-24 md:w-36 md:h-36 object-cover rounded-full h-24"
           />
           <div
@@ -44,15 +39,6 @@ export default function HeadSection({
             className={`cursor-pointer mt-14 md:mt-24 h-8 w-8 flex justify-center text-white items-center rounded-full -ml-5 md:-ml-7 ${primaryBg}`}
           >
             <FaCamera size="1.2rem"/>
-=======
-            className="w-24 rounded-full h-24"
-          />
-          <div
-            onClick={() => wrapperRef.current.click()}
-            className="cursor-pointer mt-14 -ml-3"
-          >
-            <FaCamera size="1.5rem" className={primaryColor} />
->>>>>>> 6b022236e440ed7b9e5c6ae7d83c35ea92df14a5
           </div>
           <form className="hidden">
             <input
@@ -81,11 +67,7 @@ HeadSection.defaultProps = {
 };
 
 HeadSection.propTypes = {
-<<<<<<< HEAD
   icon: PropTypes.any,
-=======
-  icon: PropTypes.element,
->>>>>>> 6b022236e440ed7b9e5c6ae7d83c35ea92df14a5
   leader: PropTypes.string,
   follower: PropTypes.string,
   showUploadPhoto: PropTypes.bool,
