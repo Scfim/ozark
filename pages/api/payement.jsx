47a07b811ad0327c4dada5cf5aab8d8f69e6fc7d
onlykal <onlykal64@gmail.com>
2021-08-18 11:50:31 +0200

    paiement api

---
+++ pages/api/payement.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import {server} from "../../constants/common"
+
+export const addPayement=()=>{
+    const url=`${server}/....`
+    return new Promise((resolve,reject)=>{
+        axios.post(url,data).then((res)=>resolve(res)).catch((err)=> reject(err))
+    })
+}
+export const editPayement=()=>{
+    const url=`${server}/....`
+    return new Promise((resolve,reject)=>{
+        axios.post(url,data).then((res)=>resolve(res)).catch((err)=> reject(err))
+    })
+}
+export const deletePayement=()=>{
+    const url=`${server}/....`
+    return new Promise((resolve,reject)=>{
+        axios.post(url,data).then((res)=>resolve(res)).catch((err)=> reject(err))
+    })
+}
+export const getPayement=()=>{
+    const url=`${server}/....`
+    return new Promise((resolve,reject)=>{
+        axios.post(url).then((res)=>resolve(res)).catch((err)=> reject(err))
+    })
+}
--- pages/payement/Payement.jsx
+++ pages/payement/Payement.jsx
@@ -8,69 +8,70 @@ import { Button } from '../../components/s/button'
 
 const Payement = () => {
 
-useEffect(()=>{
-      var today=new Date();
-      setTime(today.getHours() +':'+ today.getMinutes())       
-      
-},[])
+    useEffect(() => {
+        var today = new Date();
+        setTime(today.getHours() + ':' + today.getMinutes())
 
-    const [date ,setDate]=useState("")
-    const onSetDate=(e)=>{
-           setDate(e.target.value)
-           console.log(time)
-    }
-    const [mount ,setMount]=useState("")
-    const onSetMount=(e)=>{
-           setMount(e.target.value)         
-    }
-    const [envoy ,setEnvoy]=useState("")
-    const onSetEnvoy=(e)=>{
-           setEnvoy(e.target.value)         
-    }
-    const [time ,setTime]=useState("")
-   
-    const [bookingState ,setBookingState]=useState("hidden")
-    const [booking ,setBooking]=useState("")
-    const [bookingData ,setBookingData]=useState([])
-    const onSetBooking=(e)=>{
-           setBooking(e.target.value)
-           if(e.target.value!=""){               
-               const data = [
-                { bookingId: "12EC", bookingDesignation: "Garoumie", bookingDate:'21-09-2021' },
-                { bookingId: "42HB", bookingDesignation: "Kapouchino", bookingDate:'01-09-2021' }
-               ]
-               setBookingData(data)
-               setBookingState("")
-           }else setBookingState("hidden")
-    }
+    }, [])
 
-    const GetBooking=(bookingId,bookingDesignation,bookingDate)=>{                           
-                console.log(bookingId,bookingDesignation,bookingDate)  
-                setBookingState("hidden")  
+    const [date, setDate] = useState("")
+    const onSetDate = (e) => {
+        setDate(e.target.value)
+        console.log(time)
+    }
+    const [mount, setMount] = useState("")
+    const onSetMount = (e) => {
+        setMount(e.target.value)
+    }
+    const [envoy, setEnvoy] = useState("")
+    const onSetEnvoy = (e) => {
+        setEnvoy(e.target.value)
+    }
+    const [time, setTime] = useState("")
+
+    const [bookingState, setBookingState] = useState("hidden")
+    const [booking, setBooking] = useState("")
+    const [bookingData, setBookingData] = useState([])
+    const onSetBooking = (e) => {
+        setBooking(e.target.value)
+        if (e.target.value != "") {
+            const data = [
+                { bookingId: "12EC", bookingDesignation: "Garoumie", bookingDate: '21-09-2021' },
+                { bookingId: "42HB", bookingDesignation: "Kapouchino", bookingDate: '01-09-2021' }
+            ]
+            setBookingData(data)
+            setBookingState("")
+        } else setBookingState("hidden")
     }
 
-    
-    const AddTransaction=()=>{
-        console.log("k;jusgd;ukjds")
+
+    const [bookingId, setBookingId] = useState("")
+    const GetBooking = (bookingId, bookingDesignation, bookingDate) => {
+        setBookingId(bookingId)
+        setBooking(bookingDesignation)
+        setBookingState("hidden")
+    }
+
+
+    const AddPayement = () => {
+        console.log(bookingId, date, time, mount, envoy)        
     }
 
     return <div className={` flex flex-col bg-gray-4 my-6 justify-center items-center  `}>
         <div className={` flex flex-col bg-gray-4 my-6 w-auto `}>
             <div className={` flex flex-col w-full`}>
                 <label className={` text-xl font-bold`}> Transaction de paiement </label>
-                {/* <label className={` text-sm font-normal ${style.text}`}>  </label> */}
             </div>
             <div className={` flex flex-col w-full mt-5`}>
-                {/* <Input type="text" htmlFor="designationId" name="designation" label="Désignation" event={onSetProduct} icon={<BiNavigation size="0.95rem" />} /> */}
                 <Dropdown state={bookingState} type="text" htmlFor="bookingId" name="booking" label="N° de la commande" value={booking} event={onSetBooking}>
                     {bookingData.map((value) => {
-                        return <div key={value.bookingId} className={`text-xs  cursor-pointer py-1 px-2 ${style.bgHovered}`} onClick={() => GetBooking(value.bookingId, value.bookingDesignation,value.bookingDate)}>{value.bookingDesignation}</div>
+                        return <div key={value.bookingId} className={`text-xs  cursor-pointer py-1 px-2 ${style.bgHovered}`} onClick={() => GetBooking(value.bookingId, value.bookingDesignation, value.bookingDate)}>{value.bookingDesignation}</div>
                     })}
                 </Dropdown>
                 <Input type="date" htmlFor="dateId" name="date" label="Date de l'opération" event={onSetDate} />
                 <Input type="number" htmlFor="mountId" name="mount" label="Montant payé" event={onSetMount} icon={<BiMoney size="0.95rem" />} />
                 <Input type="text" htmlFor="envoyId" name="envoy" label="Envoyé" event={onSetEnvoy} icon={<BiNavigation size="0.95rem" />} />
-                <Button text={'Enregistrer la Transaction'} event={() => AddTransaction()} />
+                <Button text={'Enregistrer la Transaction'} event={() => AddPayement()} />
             </div>
         </div>
 
